87c977e8418526e7cdc1ec5983aad48d823d3d5d
David Gowrie <david.gowrie@gmail.com>
2014-10-30 17:37:32 -0400

    adding jquery dependency, dropping in img dim detect script - for refactoring for testing

--- src/js/imgdimdetect.js
+++ src/js/imgdimdetect.js
@@ -1,12 +1,55 @@
-define(function() {
+define(['jquery'], function($) {
   return {
-    imageDimDetect : function(URL) {
+    imageDimDetect : function(feedDatum) {
 
-        var src = URL;
+        var src = feedDatum.mediaTile;
+        var that = this;
 
         function test() {
             return 'image src path: ' + this.src;
         }
+
+
+        function getPromise(theDatum) {
+
+            // theDatum is passed in to create a closure of sorts for the entire feedDatum obj
+            var deferred = $.Deferred(),
+                img = new Image();
+
+            img.onload = function() {
+                deferred.resolve({ // 'thepromise' to be returned
+                    item: theDatum,
+                    w: img.width,
+                    h: img.height
+                });
+            };
+
+            img.src = theDatum.mediaTile;
+
+            return deferred.promise();
+
+        }
+
+        $.when(getPromise(feedDatum)).done(function(thepromise) {
+            var thatDatum = thepromise.item;
+            thatDatum.mediaTileW = thepromise.w;
+            thatDatum.mediaTileH = thepromise.h;
+            console.log('inside Promise -> feedDatum.mediaTileW: ', thatDatum.mediaTileW);
+
+            // check if mediaOrient not set
+            if (thatDatum.mediaOrient === '') {
+                thatDatum.mediaOrient = that.getMediaOrient(thatDatum);
+            }
+                        
+            // Push each post to data collection
+            that.addItems(thatDatum);
+
+            that.itemCounter(thatDatum.id);
+            
+        });
+
+
+
 
 
     	return {
