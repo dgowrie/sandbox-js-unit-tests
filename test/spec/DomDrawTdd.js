d7ee7bbdf3e26153bb33f9bc32e149b19116a464
David Gowrie <david.gowrie@gmail.com>
2014-11-03 16:12:01 -0500

    stubbing out test for DOM Drawing TDD

---
+++ test/spec/DomDrawTdd.js
@@ -0,0 +1,16 @@
+define(['DateRefactor'], function (DateRefactor) {
+
+    describe('DOM Drawing TDD demo', function () {
+
+        it('should be an object', function () {
+            expect(DateRefactor).to.be.an('object');
+        });
+
+        it('does nothing', function() {
+            expect(true).to.equal(false);
+        });
+
+        
+    });
+
+});
